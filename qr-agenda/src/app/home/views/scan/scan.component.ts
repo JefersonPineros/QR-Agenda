37ece5bbf4a51ee56f5f98e1b2b132dc8dabaf2b
Jeferson Piñeros <jpinerpr@emeal.nttdata.com>
2025-06-08 21:25:36 -0500

    fix: ajuste scan ejemplo

--- qr-agenda/src/app/home/views/scan/scan.component.ts
+++ qr-agenda/src/app/home/views/scan/scan.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonInput,
+} from '@ionic/angular/standalone';
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.scss'],
+  imports: [
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenu,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonCol,
+    IonGrid,
+    IonRow,
+    IonInput,
+  ],
 })
-export class ScanComponent  implements OnInit {
-
-  constructor() { }
+export class ScanComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
