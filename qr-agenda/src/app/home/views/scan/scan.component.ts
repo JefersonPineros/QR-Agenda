2ec0988b404cdb781a0acdb41ffe25740b284351
Nikol <143750294+nar-ran@users.noreply.github.com>
2025-06-23 09:47:40 -0500

    scan base

--- qr-agenda/src/app/home/views/scan/scan.component.ts
+++ qr-agenda/src/app/home/views/scan/scan.component.ts
@@ -1,37 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+ import { CommonModule } from '@angular/common';
 import {
-  IonButtons,
-  IonContent,
   IonHeader,
-  IonMenu,
-  IonMenuButton,
-  IonTitle,
   IonToolbar,
-  IonCol,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonCard,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonList,
   IonGrid,
   IonRow,
-  IonInput,
+  IonCol,
+  IonCheckbox,
+  IonText,
+  IonAlert,
 } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+
+interface EventData {
+  capacity: number;
+  alerts: string[];
+  scanResult: string;
+}
+
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.scss'],
   imports: [
-    IonButtons,
-    IonContent,
+    CommonModule,
     IonHeader,
-    IonMenu,
-    IonMenuButton,
-    IonTitle,
     IonToolbar,
-    IonCol,
-    IonGrid,
-    IonRow,
-    IonInput,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonAlert,
   ],
+  standalone: true,
 })
 export class ScanComponent implements OnInit {
-  constructor() {}
+  eventData: EventData = {
+    capacity: 200,
+    alerts: [
+      'Esta persona ya ha entrado',
+      'Entrada duplicada',
+      'Capacidad alcanzada',
+      'Codigo escaneado correctamente',
+    ],
+    scanResult: 'John',
+  };
+
+  isAlertOpen = false;
+  alertButtons = ['OK'];
+
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  goBack() {
+    this.router.navigate(['/home']);
+  }
+
+  onScanCode() {
+    console.log('Escaneando código...');
+  }
+
+  finalizarEvento() {
+    this.isAlertOpen = true;
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
 }
